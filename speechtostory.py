f72b882bdbb8857fa3934d5d2a32804283715898
grainDetector <malaybehera45@gmail.com>
2023-12-06 12:24:48 +0530

    story gen llm

--- speechtostory.py
+++ speechtostory.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify , json
 import speech_recognition as sr
-
+from storygeneration import storygen
 app = Flask(__name__)
 
 @app.route("/storygeneration", methods=['GET'])
@@ -12,16 +12,18 @@ def get_audio():
         try:
             said = r.recognize_google(audio)
             print(said)
+            req_output = storygen(said)
         except Exception as e:
             print('Exception: ' + str(e))
 
-    return said
+    return req_output
+
 
 @app.route("/")
 def welcome():
 	return 'welcome to UserStory Generation'
 
-    
+
 if __name__ == "__main__":
 	app.run(debug=True)
      
