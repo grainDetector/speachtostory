c06de47d1bff922094ae1aafb8f15f53cbdf7750
grainDetector <malaybehera45@gmail.com>
2023-12-06 12:24:37 +0530

    story gen

---
+++ storygeneration.py
@@ -0,0 +1,18 @@
+import openai
+# OpenAI API Key
+API_key = "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"
+openai.api_key = API_key
+
+def storygen(prompt):
+    print(prompt)
+    query = openai.Completion.create(
+		engine="text-davinci-003",
+		prompt=f"create an acceptance criteria in Gherkins from the below requirements\n {prompt}",
+		max_tokens=1024,
+		n=1,
+		stop=None,
+		temperature=0.7,
+	)
+    response = query.choices[0].text
+    print(response)
+    return response
